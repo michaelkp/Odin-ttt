58b87259edef4b3422277b35c8572b7fd8bb888c
Michael <michaelk99@gmail.com>
2022-12-13 06:12:54 -0800

    Add display and display.getMark to display player marks.

--- scripts.js
+++ scripts.js
@@ -2,24 +2,24 @@ const gameBoard = (() => {
 
     let boardBoxes = []
     let boxId = 1
+    let box
 
     const _main = document.querySelector('main')
     const _board = document.createElement('div')
         _board.className = 'gameBoard'
         _main.appendChild(_board)
 
-    const makeBoxes = () => {
+    const makeBoxes = (box) => {
         for(let i = boardBoxes.length; i <= 8; i++) {
-        const box = document.createElement('div')
+            box = document.createElement('div')
             box.classList.add('boardBox')
             box.id = boxId++
             _board.appendChild(box)
-            boardBoxes.push(box) 
-
-            
+            boardBoxes.push(box)       
     }}
 
-    return {boardBoxes, makeBoxes}
+
+    return {boardBoxes, makeBoxes, box}
 })()
 
 const players = (name, mark, turn, score) => {
@@ -36,7 +36,6 @@ const player2 = players('Two', 'O', false)
 
 const gamePlay = (() => {
     gameBoard.makeBoxes()
-
     for (const box of gameBoard.boardBoxes) {
         box.addEventListener('pointerup', () => {
             if(box.classList.contains('played')) {
@@ -44,13 +43,14 @@ const gamePlay = (() => {
             } else {
                 console.log(box.id);
                 box.classList.add('played')
-                box.textContent = player1.mark || player2.mark 
-                togglePlayerTurn()
+                // box.textContent = player1.mark || player2.mark
+                display.getMark(box)
+                _togglePlayerTurn()
             }       
         })     
     }
-// player1 has a turn
-    const togglePlayerTurn = () => {
+
+    const _togglePlayerTurn = () => {
         if(player1.turn === true) {
             console.log('Player 1');
             player1.turn = !player1.turn
@@ -64,9 +64,20 @@ const gamePlay = (() => {
             return 
         }
     }
-// then player2 has a turn
 
 // then find position of marks and see if it matches win condiitons
 
     return {  }
+})()
+
+const display = (() => {
+
+    const getMark = (box) => {
+        console.log(box.id);
+
+        if(player1.turn === true) return box.textContent = player1.mark
+        if(player2.turn === true) return box.textContent = player2.mark
+    } 
+
+    return { getMark }
 })()
